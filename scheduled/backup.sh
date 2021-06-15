60350fd86f196ddc565e578b9eb0c680a62a6f99
Jakub Partyka <kontakt@jakub-partyka.pl>
2021-06-15 10:40:22 +0200

    Update backup.sh

--- scheduled/backup.sh
+++ scheduled/backup.sh
@@ -10,8 +10,19 @@ source="/root /deployment /backup/archives"
 # where to save the backup
 dest="/backup/automatic"
 
-fn=$(date +%Y.%m.%d)_backup.tar.gz
+# tmp filename
+tmp="backup.tmp.tar.gz"
 
-tar czvf $dest/$fn $source
+fn=$(date +%Y_%m_%d_%H_%M_%S)_backup.tar.gz
+
+tar czvf $dest/$tmp $source
+
+if [[ $? -ne 0 ]] ; then
+	echo "failed to create backup, aborting"
+fi
+
+# remove previous backup files
+rm "$dest"/*_backup.tar.gz
+mv $dest/$tmp $dest/$fn
 
 echo "backup created"
